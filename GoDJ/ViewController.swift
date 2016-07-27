733350043ba481d308ae4b15999fb379d31eb531
Ari Bolton <ariwbolton@gmail.com>
2016-07-26 22:01:00 -0700

    Initial Commit

---
+++ GoDJ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoDJ
+//
+//  Created by Elwood Bolton on 7/26/16.
+//  Copyright © 2016 Ari Bolton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
